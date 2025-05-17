442068bd6f6d50bec9acda87ed15039e87a346f3
maks_smirnov <smirnovmaks05@yandex.ru>
2025-05-17 21:42:46 +0300

    fix bugs

--- main/internal/implementation/blockchain_client.go
+++ main/internal/implementation/blockchain_client.go
@@ -1,6 +1,9 @@
 package implementation
 
 import (
+	"errors"
+	"log"
+
 	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -11,21 +14,20 @@ type BlockchainClient struct {
 }
 
 func NewBlockchainClient(ethRPC, btcHost, btcUser, btcPass string) (*BlockchainClient, error) {
-	return nil, nil
-	// ethClient, err := ethclient.Dial(ethRPC)
-	// if err != nil {
-	// 	return nil, errors.New("failed to connect to Ethereum node")
-	// }
-	// btcClient, btcErr := rpcclient.New(&rpcclient.ConnConfig{
-	// 	Host:         btcHost,
-	// 	User:         btcUser,
-	// 	Pass:         btcPass,
-	// 	HTTPPostMode: true,
-	// 	DisableTLS:   true,
-	// }, nil)
-	// if btcErr != nil {
-	// 	log.Println("failed to connect to Bitcoin node")
-	// 	btcClient = nil
-	// }
-	// return &BlockchainClient{EthClient: ethClient, BtcClient: btcClient}, nil
+	ethClient, err := ethclient.Dial(ethRPC)
+	if err != nil {
+		return nil, errors.New("failed to connect to Ethereum node")
+	}
+	btcClient, btcErr := rpcclient.New(&rpcclient.ConnConfig{
+		Host:         btcHost,
+		User:         btcUser,
+		Pass:         btcPass,
+		HTTPPostMode: true,
+		DisableTLS:   true,
+	}, nil)
+	if btcErr != nil {
+		log.Println("failed to connect to Bitcoin node")
+		btcClient = nil
+	}
+	return &BlockchainClient{EthClient: ethClient, BtcClient: btcClient}, nil
 }
